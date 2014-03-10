6a08213a59d4e72cdc3c66526f55b98af267e1be
nika-nalle <nikaandreenkova@yandex.ru>
2014-03-10 21:29:20 +0400

    First file

---
+++ u1.php
@@ -0,0 +1,19 @@
+﻿<html>
+<form action="u1.php" method="POST">
+Name: <input type="text" name="name"/><br/>
+Textarea: <input type name="text"/>
+<p><input type="submit" value=" Отправить "></p>
+<?php
+$name = $_POST['name'];
+$textarea = $_POST['text'];
+echo $name;
+$posts[0] = array ("title" => 'Hello', 'Hey'); 
+$posts[1] = array ("message" => 'Helloo', 'Yo Hey');
+var_dump($posts);
+
+
+if ($_POST) {
+$posts[] = array (
+'title' = $_POST['text'];
+?>
+</html>
